59a7e0cee12d13502d09b9d94dcbea3559cbf976
AndrewGalkin123 <127788127788a@gmail.com>
2023-11-28 10:23:03 +0200

    more content

--- src/App.js
+++ src/App.js
@@ -12,18 +12,29 @@ import "./App.css"
 const App = () => {
   const methods = content.map(item => item.method);
   const descriptions = content.map(item => item.description);
-  const examples = content.map(item => item.examples)
+  const basicExamples = content.map(item => item.basicExample)
+  const basicExampleResults = content.map(item => item.basicExampleResult)
+  const syntaxes = content.map(item => item.syntax)
+  const cycleExamples = content.map(item => item.cycleExample)
+  const cycleExampleResults = content.map(item => item.cycleExampleResult)
   const [searchedMethods, setSearchedMethods] = useState(methods)
   const [selectedMethod, setSelectedMethod] = useState(methods[0]);
   const [description, setDescription] = useState(descriptions[0]);
-  const [example, setExample] = useState(examples[0]);
+  const [syntax, setSyntax] = useState(syntaxes[0]);
+  const [basicExample, setBasicExample] = useState(basicExamples[0]);
+  const [basicExampleResult, setBasicExampleResult] = useState(basicExampleResults[0]);
+  const [cycleExample, setCycleExample] = useState(cycleExamples[0]);
+  const [cycleExampleResult, setCycleExampleResult] = useState(cycleExampleResults[0]);
 
   const handleMenuSelect = (selectedMethod) => {
     const index = methods.indexOf(selectedMethod);
     setSelectedMethod(selectedMethod);
     setDescription(descriptions[index]);
-    setExample(examples[index])
-
+    setBasicExample(basicExamples[index])
+    setSyntax(syntaxes[index])
+    setBasicExampleResult(basicExampleResults[index])
+    setCycleExample(cycleExamples[index])
+    setCycleExampleResult(cycleExampleResults[index])
   };
 
   const handleMenuSearch = (value) => {
@@ -42,13 +53,21 @@ const App = () => {
 
               </Col>
               <Col xs={14} md={21}>
-                <Description example={example} title={selectedMethod} description={description} />
+                <Description
+                  syntax={syntax}
+                  basicExample={basicExample}
+                  title={selectedMethod}
+                  description={description}
+                  basicExampleResult={basicExampleResult}
+                  cycleExample={cycleExample}
+                  cycleExampleResult={cycleExampleResult}
+                />
               </Col>
             </Row>
           </>
         }>
         </Route>
-        <Route path='/about-us' element={<AboutUs/>}/>
+        <Route path='/about-us' element={<AboutUs />} />
       </Routes>
     </Router>
   );
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,15 +1,36 @@
 import { Divider, Typography } from 'antd'
 
-const Description = ({ title, description, example }) => {
-
-
+const Description = ({ title, description, syntax, basicExample, basicExampleResult, cycleExample, cycleExampleResult }) => {
     return (
         <div style={{ padding: '20px' }}>
             <Typography.Title level={2}>{title}</Typography.Title>
             <Divider />
-            <Typography.Paragraph style={{fontSize: "18px"}}>{description}</Typography.Paragraph>
+            <Typography.Paragraph style={{ fontSize: "18px" }}>{description[0]}</Typography.Paragraph>
 
-            {example.map(el => <><Typography.Text code>{el}</Typography.Text><br/></>)}
+
+
+            <Typography.Title level={3}>Синтаксис:</Typography.Title>
+            <div className='code'>
+                {syntax}
+            </div><br></br>
+            <Typography.Title level={3}>Базовый пример:</Typography.Title>
+            <div className="code">
+                {basicExample.map(el => <>{el}<br/></>)}
+            </div><br />
+            <Typography.Title level={3}>Результат:</Typography.Title>
+            <div className="code">
+                {basicExampleResult.map(el => <>{el}<br/></>)}
+            </div><br />
+            <Typography.Title level={3}>Использование в цикле:</Typography.Title>
+            <div className="code">
+                {cycleExample.map(el => <>{el}<br/></>)}
+            </div><br />
+            <Typography.Title level={3}>Результат:</Typography.Title>
+            <div className="code">
+                {cycleExampleResult.map(el => <>{el}<br/></>)}
+            </div><br />
+            <Typography.Paragraph style={{ fontSize: "18px" }}>{description[1]}</Typography.Paragraph>
+
 
 
         </div>
