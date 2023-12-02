fd7146e05eea0401316452802cd5577451b208b9
AndrewGalkin123 <127788127788a@gmail.com>
2023-12-02 05:34:18 +0200

    logo-link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({title}) => {
         <header style={{ padding: "20px 20px 0" }}>
             <Row style={{ display: "flex", alignItems: "center" }}>
                 <Col xs={21}>
-                    <Typography.Title level={2}>{title}</Typography.Title>
+                <a href='/'><Typography.Title level={2}>{title}</Typography.Title></a>
                 </Col>
                 <Col xs={3}>
                     <Navigation />
